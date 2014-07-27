0fb8403c33569becf534591e6d739f37572e3341
Bruno Quint <bruno.quint@iag.usp.br>
2014-07-27 13:50:01 -0300

    Created script to install IRAF. Not functional yet but milestones are inside it.

---
+++ install_iraf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## 
+## IRAF Install Script 
+##
+## Run this script in a terminal once you have finished your Ubuntu 14.04 or
+## Linux Mint 17 installation to install IRAF in your system.
+##
+echo ""
+echo " IRAF Install Script."
+echo " Department of Astronomy - IAG/USP"
+echo " by Bruno Quint <bquint at astro.iag.usp.br"
+echo ""
+
+## Test if there is a previous IRAF installation in your machine first
+
+## If found, do you want to remove it?
+
+## If not, do you really want to proceed with installation?
+
+## What is your processor's architecture?
+
+## Where do you want to save the files?
+
+## Download files from IRAF's webside
+
+## Install it
+
+## Do you want to remove installation files?
+
+
