8c594767713fa47cbdc6d3317f7490d6e741de59
Bruno  Quint <b1quint@gmail.com>
2014-07-30 19:58:26 +0000

    after_install.sh edited online with Bitbucket

--- after_install.sh
+++ after_install.sh
@@ -43,29 +43,39 @@ sudo aptitude install \
  okular \
  remmina remmina-plugin-vnc \
  nfs-common nfs-client nfs-server \
- saods9 \
+ csh saods9 \
  vpnc \
  xorg-dev \
  libx11-dev \
  virtualbox virtualbox-guest-utils \
  p7zip-full atool 
 
-echo " Installing Oracle Java"
-sudo apt-get purge openjdk*
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java7-installer
-
-echo " Downloading and installing QFitsView for FITS data visualization." 
-wget http://www.mpe.mpg.de/~ott/QFitsView/QFitsView_3.1.linux64 
-sudo chmod a+x QFitsView_3.1.linux64
-sudo cp QFitsView_3.1.linux64 /usr/bin/QFitsView
-sudo ln -s /usr/bin/QFitsView /usr/bin/qfitsview
-rm QFitsView_3.1.linux64
-
+echo ""
+echo " Installing/Upgrading Python Libraries"
+echo ""
 sudo pip install numpy --upgrade
 sudo pip install scipy --upgrade
 sudo pip install matplotlib --upgrade
 sudo pip install astropy --upgrade
 sudo pip install watchdog --upgrade
 sudo pip install argparse --upgrade
+
+echo ""
+echo " Installing Oracle Java"
+echo ""
+sudo apt-get purge openjdk*
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+
+echo ""
+echo " Downloading and installing QFitsView for FITS data visualization." 
+echo ""
+
+wget http://www.mpe.mpg.de/~ott/QFitsView/QFitsView_3.1.linux64 
+sudo chmod a+x QFitsView_3.1.linux64
+sudo cp QFitsView_3.1.linux64 /usr/bin/QFitsView
+sudo ln -s /usr/bin/QFitsView /usr/bin/qfitsview
+rm QFitsView_3.1.linux64
+
+
